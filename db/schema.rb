3e59ad0ddde8394522a77ae68fde929f97af41c2
Mitesh Patel <mpatel5@icloud.com>
2017-04-16 02:05:00 +0000

    Fixed migration files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170416012140) do
+ActiveRecord::Schema.define(version: 20170416020118) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
